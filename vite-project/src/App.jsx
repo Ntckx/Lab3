ba2b45d8b10ac9d631665eba87960f2d2b26ff97
ntck <nontakorn.chatk@gmail.com>
2023-03-27 15:41:58 +0700

    First commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Home from "./components/Home";
+import Todo from "./components/Todo";
+import TodoPage from "./pages/TodoPage";
+function App(){
+    return(
+        <TodoPage/>
+    );
+}
+export default App;
---
+++ vite-project/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+
+import "./Profile.css";
+function Profile(){
+    return(
+        <div className="layout">
+            <img src="assets/AB71AF18-B9A2-4072-89C0-4B95A9FE1C46.png" alt="profile" />
+            <h1 className="name">Nontakorn Chatkoonsathien</h1>
+            <hr></hr>
+        </div>
+    )
+}
+export default Profile;
